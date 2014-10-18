4e4b0f5f43fdbabbf24a53c0d2fcec73ec269a13
afspear <afspear@gmail.com>
2014-10-18 14:59:36 -0500

    added MotionDectector and RealTimeFeed

---
+++ src/main/java/com/spear/home/control/MotionDetector.java
@@ -0,0 +1,12 @@
+package com.spear.home.control;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class MotionDetector {
+
+	public AtomicBoolean getDetecting() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/spear/home/control/RealtimeFeed.java
@@ -0,0 +1,21 @@
+package com.spear.home.control;
+
+import java.io.InputStream;
+
+import com.vaadin.server.StreamResource.StreamSource;
+
+public class RealtimeFeed implements Runnable, StreamSource {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public InputStream getStream() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
